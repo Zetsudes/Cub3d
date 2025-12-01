b8e9acad6bb130c74b91c788c7188a3db8ef9ba8
Zainab Mohamed <zamohame@c1r7p1.42nice.fr>
2025-12-01 13:21:07 +0100

    minilibx

--- include/raycasting.h
+++ include/raycasting.h
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   raycasting.h                                       :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: zetsu <zetsu@student.42.fr>                +#+  +:+       +#+        */
+/*   By: zamohame <zamohame@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/10/30 10:58:10 by zamohame          #+#    #+#             */
-/*   Updated: 2025/11/28 21:01:58 by zetsu            ###   ########.fr       */
+/*   Updated: 2025/12/01 13:20:29 by zamohame         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -26,7 +26,7 @@
 # define win_height 600
 # define FOV (M_PI / 3)
 
-# include "../minilibx-linux/mlx.h"
+# include "../target/minilibx/mlx.h"
 # include <X11/keysym.h>
 # include <math.h>
 # include <stddef.h>
